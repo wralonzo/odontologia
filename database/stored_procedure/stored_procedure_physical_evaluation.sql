1ccabb50a877323707a3c25681063edfd100437a
GEAT <gersontajiboy8@gmail.com>
2024-08-13 15:52:18 -0600

    UPDATE
    UPDATE

--- database/stored_procedure/stored_procedure_physical_evaluation.sql
+++ database/stored_procedure/stored_procedure_physical_evaluation.sql
@@ -23,35 +23,3 @@ BEGIN
     COMMIT;
 END //
 DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE procedure_to_update_physical_evaluation(
-    IN p_id INT,
-    IN p_blood_pressure VARCHAR(10),
-    IN p_blood_sugar VARCHAR(10),
-    IN p_last_treatment TEXT,
-    IN p_other_data TEXT,
-    IN p_patient_id INT
-)
-BEGIN 
-    DECLARE v_existing_status BOOLEAN;
-    SELECT IFNULL(status, TRUE) INTO v_existing_status
-    FROM physical_evaluation
-    WHERE id = p_id;
-    IF v_existing_status IS NULL THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Physical evaluation item does not exist.';
-    ELSEIF v_existing_status = FALSE THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Physical evaluation item exists but is logically deleted.';
-    ELSE
-        UPDATE physical_evaluation
-        SET 
-            blood_pressure = p_blood_pressure,
-            blood_sugar = p_blood_sugar,
-            last_treatment = p_last_treatment,
-            other_data = p_other_data,
-            updatedAt = NOW(),
-            patient_id = p_patient_id
-        WHERE id = p_id;
-    END IF;
-END //
-DELIMITER ;
