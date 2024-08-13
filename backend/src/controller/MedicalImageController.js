c9d05f05d121d15ea745984d737c00ef353624d0
GEAT <gersontajiboy8@gmail.com>
2024-08-13 17:01:11 -0600

    UPDATE
    UPDATE

--- backend/src/controller/MedicalImageController.js
+++ backend/src/controller/MedicalImageController.js
@@ -4,27 +4,29 @@ const sequelize = MedicalImage.sequelize;
 
 export const registerMedicalImage = async (req, res, next) => {
   const transaction = await sequelize.transaction();
-  try {
-    const {
-      image_base_64,
-      description,
-      patient_id
-    } = req.body;
-
-    await sequelize.query('CALL procedure_to_register_medical_image(:image_base_64, :description, :patient_id)', {
-      replacements: {
-        image_base_64,
-        description,
-        patient_id
-      },
-      transaction: transaction
-    });
-
-    await transaction.commit();
-    res.json({ message: 'Medical image registered successfully.' });
-  } catch (error) {
-    await transaction.rollback();
-    console.error('Error registering medical image.', error);
-    res.status(500).send('Internal Server Error.');
-  }
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({ message: 'Error parsing the form data.' });
+    }
+    try {
+      const { description, patient_id } = fields;
+      const { image_base_64 } = files;
+      const imageBuffer = await fs.promises.readFile(image_base_64.path);
+      await sequelize.query('CALL procedure_to_register_medical_image(:image_base_64, :description, :patient_id)', {
+        replacements: {
+          image_base_64: imageBuffer,
+          description,
+          patient_id
+        },
+        transaction: transaction
+      });
+      await transaction.commit();
+      res.json({ message: 'Medical image registered successfully.' });
+    } catch (error) {
+      await transaction.rollback();
+      console.error('Error registering medical image.', error);
+      res.status(500).send('Internal Server Error.');
+    }
+  });
 };
